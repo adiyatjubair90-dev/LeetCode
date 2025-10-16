dc9dc2a46957c71771c98aea77a8283c5698177e
AdiyatJ06 <adiyatjubair90@gmail.com>
2025-10-16 17:48:09 -0400

    Update RemoveDuplicates.py

---
+++ RemoveDuplicates.py
@@ -0,0 +1,9 @@
+def removeDuplicates(self, nums):
+    if len(nums) == 0:
+        return 0
+    k = 0
+    for i in range(1, len(nums)):
+        if nums[k] != nums[i]:
+            k+=1
+            nums[k] = nums[i]
+    return k+1
